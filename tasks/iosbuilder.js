7a31ba960e9cd197995c5141a326d6e5bd1bd9e2
Francois Dumas <devfd@users.noreply.github.com>
2014-07-07 13:41:54 +0200

    prebuild script executed before starting a build. Change app name base on build mode

--- tasks/iosbuilder.js
+++ tasks/iosbuilder.js
@@ -10,15 +10,21 @@ module.exports = function(grunt) {
         cwd = path.resolve(config.path) || process.cwd();
 
     IosBuilder.create(cwd).then(function(ios) {
-      ios.updateProjectInfo(data.appId)
+      ios.updateProjectInfo({
+        appId: data.appId,
+        appMode: data.appMode,
+        displayName: config.displayName
+      })
       .then(function() {
         return ios.build({
           appId: data.appId,
+          appMode: data.appMode,
           scheme: data.scheme || config.scheme,
           configuration: data.configuration || config.configuration,
           sdk: data.sdk || config.sdk,
           profileId: data.profileId,
-          identity: data.identity
+          identity: data.identity,
+          prebuild: data.prebuild || config.prebuild
         })
       })
       .then(done);
@@ -32,17 +38,23 @@ module.exports = function(grunt) {
         cwd = path.resolve(config.path) || process.cwd();
 
     IosBuilder.create(cwd).then(function(ios) {
-      ios.updateProjectInfo(data.appId)
+      ios.updateProjectInfo({
+        appId: data.appId,
+        appMode: data.appMode,
+        displayName: config.displayName
+      })
       .then(function() {
         return ios.exportIpa({
           appId: data.appId,
+          appMode: data.appMode,
           archiveName: data.archiveName,
           scheme: data.scheme || config.scheme,
           configuration: data.configuration || config.configuration,
           profileId: data.profileId,
+          profileName: data.profileName,
           identity: data.identity,
           ipaName: data.ipaName,
-          profileName: data.profileName
+          prebuild: data.prebuild || config.prebuild
         });
       })
       .then(done);
