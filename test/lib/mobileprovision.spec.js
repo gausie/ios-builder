f47e983765a2ad65df5d4263a54d169ef9a7531f
devfd <devfd@users.noreply.github.com>
2014-07-21 16:21:36 +0200

    MobileProvision specs update

--- test/lib/mobileprovision.spec.js
+++ test/lib/mobileprovision.spec.js
@@ -3,6 +3,7 @@ var Promise = require('bluebird'),
     readFile = require('fs').readFileSync,
     Security = req('security'),
     readFile = require('fs').readFileSync,
+    plist = require('plist'),
     MobileProvision = req('MobileProvision');
 
 describe('MobileProvision', function() {
@@ -36,5 +37,72 @@ describe('MobileProvision', function() {
     });
   }); // end _unwrapApple
 
-  
+  describe('_extractProfileData', function() {
+
+    it('extract profile data', function() {
+      var _certToPem = sinon.stub(MobileProvision, '_certToPem'),
+          _pemToPub = sinon.stub(Security, 'pemToPub'),
+          data = readFile('test/fixtures/profile.mobileprovision', {encoding: 'utf8'});
+
+      _certToPem.returns('');
+      _pemToPub.returns(Promise.resolve('PUBKEY'));
+
+      return MobileProvision._extractProfileData(data)
+        .then(function(profile) {
+          expect(profile.name).to.eql('Test APP');
+          expect(profile.teamId).to.eql('APPID');
+          expect(profile.uuid).to.eql('123ABC12-ABCD-1234-ABCD-123456789ABC');
+          expect(profile.publicKeys).to.eql(['PUBKEY', 'PUBKEY']);
+          expect(_pemToPub).to.have.callCount(2);
+        })
+        .finally(_certToPem.restore)
+        .finally(_pemToPub.restore);
+    });
+
+    it('stores profiles by application-identifier', function() {
+      var _certToPem = sinon.stub(MobileProvision, '_certToPem'),
+          _pemToPub = sinon.stub(Security, 'pemToPub'),
+          data = readFile('test/fixtures/profile.mobileprovision', {encoding: 'utf8'});
+
+      _certToPem.returns('');
+      _pemToPub.returns(Promise.resolve('PUBKEY'));
+
+      return MobileProvision._extractProfileData(data)
+        .then(function(profile) {
+          var keys = [];
+          for(var k in MobileProvision.provisions) {
+            if(MobileProvision.provisions.hasOwnProperty(k)) {
+              keys.push(k);
+            }
+          }
+
+          expect(MobileProvision.provisions).to.exist;
+          expect(keys).to.eql(['com.iosbuilder.testappr']);
+        })
+        .finally(_certToPem.restore)
+        .finally(_pemToPub.restore);
+    });
+  }); // end _extractProfileData
+
+  describe('_certToPem', function() {
+
+    it('decodes base64 certificate', function() {
+      var data = readFile('test/fixtures/profile.mobileprovision', {encoding: 'utf8'}),
+          profile = plist.parse(data);
+
+      return expect(MobileProvision._certToPem(profile.DeveloperCertificates[0]))
+        .to.match(/BEGIN CERTIFICATE/)
+        .and.to.match(/END CERTIFICATE/);
+    });
+
+    it('is a valid certificate', function() {
+      var data = readFile('test/fixtures/profile.mobileprovision', {encoding: 'utf8'}),
+          profile = plist.parse(data),
+          cert = MobileProvision._certToPem(profile.DeveloperCertificates[0]);
+
+      return expect(Security.pemToPub('echo ' + cert))
+        .to.eventually.match(/BEGIN PUBLIC KEY/)
+        .and.eventually.to.match(/END PUBLIC KEY/);
+    });
+  }); // end _certToPem
 });
